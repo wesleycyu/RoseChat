3d2993a2e7d5ab4f45ace0e6188491f02b744b15
Wes <wesley.ch.yu@gmail.com>
2015-08-28 20:56:07 -0700

    Finish Simple Chat functionality

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", function (req, res){
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 http.listen(3000, function(){
   console.log("Listening on port 3000");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on("connection", function(socket){
+  socket.on("chat message", function(msg) {
+    io.emit('chat message', msg);
   });
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 var socket = io();
+
+socket.on("chat message", function(msg){
+  $("#messages").append($("<li>").text(msg));
+});
+
+$(function(){
+  $("form").submit(function(e) {
+    socket.emit("chat message", $("#message-box").val());
+    $("#message-box").val("");
+    return false;
+  });
+});
+
+
