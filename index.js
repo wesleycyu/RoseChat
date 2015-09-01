fc8aa8d9d8007960f283c1d3be457964881d7bbb
Wes <wesley.ch.yu@gmail.com>
2015-09-01 16:35:32 -0700

    Users must choose unique nicknames to enter chat

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
+var nicknames = []
+
 app.use(express.static("public"));
 
 app.get("/", function (req, res){
@@ -15,14 +17,32 @@ http.listen(3000, function(){
 
 io.on("connection", function(socket){
 
-  socket.broadcast.emit("user connected");
+  socket.on("new user", function(nickname, callback){
+    if (nicknames.indexOf(nickname) == -1) {
+      callback({isValid:true});
+      nicknames.push(nickname)
+      socket.nickname = nickname
+      socket.broadcast.emit("user connected", socket.nickname);
+    } else {
+      callback({
+        isValid: false,
+        errorMessage: "This nickname has already been taken!"
+      })
+    }
+  })
 
   socket.on("chat message", function(msg) {
-    io.emit("chat message", msg);
+    var chat_object = {
+      sender_name: socket.nickname,
+      message: msg
+    }
+    io.emit("chat message", chat_object);
   });
 
   socket.on('disconnect', function(){
-    io.emit("user disconnected")
+    var remove_index = nicknames.indexOf(socket.nickname)
+    nicknames.splice(remove_index, remove_index)
+    io.emit("user disconnected", socket.nickname)
   });
 
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,38 @@
 var socket = io();
 
-socket.on("chat message", function(msg){
-  $("#messages").append($("<li>").text(msg));
+socket.on("chat message", function(chat_object){
+  $("#messages").append($("<li>").text(chat_object.sender_name +": " + chat_object.message));
 });
 
-socket.on("user connected", function(){
-  $("#messages").append($("<li class='new_user_msg'>").text("A new user has connected!"))
+socket.on("user connected", function(nickname){
+  $("#messages").append($("<li class='user_connect_msg'>").text(nickname + " has connected!"))
 });
 
-socket.on("user disconnected", function(){
-  $("#messages").append($("<li class='user_left_msg'>").text("A user has disconnected!"))
+socket.on("user disconnected", function(nickname){
+  $("#messages").append($("<li class='user_disconnect_msg'>").text(nickname + " has disconnected!"))
 });
 
 $(function(){
-  $("form").submit(function(e) {
-    console.log(socket.id)
+
+  $("#chat-message-form").submit(function(e) {
     socket.emit("chat message", $("#message-box").val());
     $("#message-box").val("");
     return false;
   });
+
+  $("#nickname-form").submit(function(e){
+    socket.emit("new user", $("#nickname-input").val(), function(data){
+      if (data.isValid){
+        $("#nickname-wrap").hide();
+        $("#chat-wrap").show();
+      } else {
+        $("#nickname-errors").html(data.errorMessage);
+      }
+    });
+    $("nickname-input").val("");
+    return false
+  })
+
 });
 
 
